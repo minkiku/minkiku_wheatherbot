579e36fa430fabddbfb09aa2db9a80c57ef1b4ca
minkiku <minkiku1105@gmail.com>
2018-05-19 15:50:30 +0900

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+# consumer_key
+C_Key = " 取得した認証情報を入力 "
+
+# consumer_secret
+C_S_Key = " 取得した認証情報を入力 "
+
+# token
+Tkn = " 取得した認証情報を入力 "
+
+# token = secret
+Tkn_S = " 取得した認証情報を入力 "
